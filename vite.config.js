00273c3863e7d641634b0085e6aed7249a68205b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-07-07 08:45:56 +0530

    Update vite config to support nested pages

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
-export default {
-    base: '/30-days-of-javascript/'
-  }
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/30-days-of-javascript/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'projects/weather-app/index.html'),
+      },
+    },
+  },
+});
