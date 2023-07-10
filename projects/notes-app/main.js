347e60b9af1d99faedc2b7de32183006faaa2ae2
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-07-10 21:26:59 +0530

    Save and retrieve notes from local storage

--- projects/notes-app/main.js
+++ projects/notes-app/main.js
@@ -5,18 +5,48 @@ function $(selector) {
 const addNodeBtn = $('#add-note-btn');
 const notesElm = $('#notes');
 
+window.addEventListener('load', onLoad);
+
+function onLoad() {
+    const notes = getNotesFromLocalStorage();
+
+    for(const note of notes) {
+        const noteElm = createNoteElm(note);
+        notesElm.appendChild(noteElm);
+    }
+}
+
 addNodeBtn.addEventListener('click', handleAddNodeBtnClick);
 
 function handleAddNodeBtnClick() {
-    const newNoteElm = createNoteElm();
+    const newNoteId = btoa(+Math.random());
+    const newNoteElm = createNoteElm(newNoteId);
+    const newNote = {id: newNoteId, note: ''};
+    saveNoteToLocalStorage(newNote);
     notesElm.insertBefore(newNoteElm, notesElm.firstChild);
 }
 
-function createNoteElm() {
+function createNoteElm(note) {
     const noteElm = document.createElement('article');
+    noteElm.id = note.id;
     const textarea = document.createElement('textarea');
+    textarea.textContent = note.note;
     const deleteNoteBtn = document.createElement('button');
     deleteNoteBtn.innerHTML = `<span class="material-icons">delete</span>`;
     noteElm.append(textarea, deleteNoteBtn);
     return noteElm;
+}
+
+function getNotesFromLocalStorage() {
+    let notesString = localStorage.getItem('notes');
+    if (!notesString) return [];
+    return JSON.parse(notesString);
+}
+
+function saveNoteToLocalStorage(note) {
+    let notes = [];
+    let notesString = localStorage.getItem('notes');
+    if (notesString) note = JSON.parse(notesString);
+    notes = [note, ...notes];
+    localStorage.setItem('notes', JSON.stringify(notes));
 }
