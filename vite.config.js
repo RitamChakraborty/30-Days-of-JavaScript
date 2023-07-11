ffe6fa89ba7e44e60f706d22619fe95297597d32
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-07-11 16:51:55 +0530

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
         weather_app: resolve(__dirname, 'projects/weather-app/index.html'),
         todo_list_app: resolve(__dirname, 'projects/todo-list-app/index.html'),
         quiz_app: resolve(__dirname, 'projects/quiz-app/index.html'),
-        password_generator_app: resolve(__dirname, 'projects/password_generator_app/index.html')
+        password_generator_app: resolve(__dirname, 'projects/password-generator-app/index.html'),
+        notes_app: resolve(__dirname, 'projects/notes-app/index.html')
       },
     },
   },
