da262cbf5bc93fdcb17f1d8607a8a51bada943ac
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-07-14 19:17:17 +0530

    Create stop watch app

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,8 @@ export default defineConfig({
         quotes_app: resolve(__dirname, 'projects/quotes-app/index.html'),
         qr_code_generator_app: resolve(__dirname, 'projects/qr-code-generator-app/index.html'),
         toast_notification_app: resolve(__dirname, 'projects/toast-notification-app/index.html'),
-        music_player_app: resolve(__dirname, 'projects/music-player-app/index.html')
+        music_player_app: resolve(__dirname, 'projects/music-player-app/index.html'),
+        stop_watch_app: resolve(__dirname, 'projects/stopwatch-app/index.html')
       },
     },
   },
