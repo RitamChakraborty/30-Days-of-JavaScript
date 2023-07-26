23d19b7554d80ccd7197a71db42021c3d07ff781
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-07-26 16:52:29 +0530

    Bug fix

--- projects/text-to-speech-converter-app/main.js
+++ projects/text-to-speech-converter-app/main.js
@@ -1,16 +1,19 @@
 const voiceSelector = document.querySelector('#voice');
 const voices = new Map();
+let init = false;
 
 speechSynthesis.addEventListener('voiceschanged', () => {
+    if (init) return;
     let index = 0;
     for (const voice of speechSynthesis.getVoices()) {
         voices.set(voice.name, voice)
         const voiceOption = `<option value='${voice.name}' ${index++ === 0 ? 'selected' : ''}>${voice.name}</voice>`;
         voiceSelector.insertAdjacentHTML('beforeend', voiceOption);
     }
+    init = true;
 });
 
-document.querySelector('#listen').addEventListener('click', (e) => {
+document.forms['text-to-speech'].addEventListener('submit', (e) => {
     e.preventDefault();
     const messsage = document.querySelector('#message').value;
     if (!messsage) return;
