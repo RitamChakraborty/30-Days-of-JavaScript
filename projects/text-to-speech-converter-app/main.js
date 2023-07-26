7c1aaf07d987502c6aa1e5e0fd085e904e673a5f
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-07-26 16:39:01 +0530

    Add logic

--- projects/text-to-speech-converter-app/main.js
+++ projects/text-to-speech-converter-app/main.js
@@ -1,2 +1,20 @@
-// const utternace = new SpeechSynthesisUtterance('hello');
-// speechSynthesis.speak(utternace);
+const voiceSelector = document.querySelector('#voice');
+const voices = new Map();
+
+speechSynthesis.addEventListener('voiceschanged', () => {
+    let index = 0;
+    for (const voice of speechSynthesis.getVoices()) {
+        voices.set(voice.name, voice)
+        const voiceOption = `<option value='${voice.name}' ${index++ === 0 ? 'selected' : ''}>${voice.name}</voice>`;
+        voiceSelector.insertAdjacentHTML('beforeend', voiceOption);
+    }
+});
+
+document.querySelector('#listen').addEventListener('click', (e) => {
+    e.preventDefault();
+    const messsage = document.querySelector('#message').value;
+    if (!messsage) return;
+    const utternace = new SpeechSynthesisUtterance(messsage);
+    utternace.voice = voices.get(voiceSelector.value);
+    speechSynthesis.speak(utternace);
+});
