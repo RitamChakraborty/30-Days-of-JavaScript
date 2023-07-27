79386c44857db146b4113b067c5669985f599956
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-07-27 09:54:09 +0530

    Create image background transition app

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,8 @@ export default defineConfig({
         email_subscription_app: resolve(__dirname, 'projects/email-subscription-app/index.html'),
         password_strength_app: resolve(__dirname, 'projects/password-strength-app/index.html'),
         text_to_speech_converter_app: resolve(__dirname, 'projects/text-to-speech-converter-app/index.html'),
-        coming_soon_app: resolve(__dirname, 'projects/coming-soon-app/index.html')
+        coming_soon_app: resolve(__dirname, 'projects/coming-soon-app/index.html'),
+        image_background_transition: resolve(__dirname, 'projects/image-background-transition-app/index.html')
       },
     },
   },
