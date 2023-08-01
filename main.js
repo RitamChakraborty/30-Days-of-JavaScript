8c5a97181d8d3e288cb4a36d58c1fb8968cb0edf
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-08-02 00:00:41 +0530

    Add target balnk to code links

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ for (const project of projects) {
     tr.innerHTML = `
         <td>${project.id}</td>
         <td>${project.projectName}</td>
-        <td><a href="${project.codeLink}"><img src="https://img.icons8.com/?size=48&id=keI1M862UTP2&format=png" alt="${project.projectName} GitHub"</a></td>
+        <td><a href="${project.codeLink}" target="_blank"><img src="https://img.icons8.com/?size=48&id=keI1M862UTP2&format=png" alt="${project.projectName} GitHub"</a></td>
         <td><a href="${project.gitHubPagesLink}" target="_blank"><img src="https://img.icons8.com/?size=48&id=AZOZNnY73haj&format=png" alt="${project.projectName} GitHub Pages" </a></td>
         <td><a href="${project.netlifyLink}" target="_blank"><img src="https://img.icons8.com/?size=48&id=sBo1RJ3rjbje&format=png" alt="${project.projectName} Netlify" /></a></td>
         <td>${project.timeline}</td>
